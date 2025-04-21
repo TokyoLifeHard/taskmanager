31308e0fcff74b8dafdbf5ca041641c841f1dc90
Kawaiiskii <antonzhadnov@gmail.com>
2025-04-21 16:01:58 +0300

    implement entity User

--- src/main/java/ru/tokyolifehard/taskmanager/entity/User.java
+++ src/main/java/ru/tokyolifehard/taskmanager/entity/User.java
@@ -1,4 +1,28 @@
 package ru.tokyolifehard.taskmanager.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
 public class User {
+
+    @Id
+    @GeneratedValue()
+    private Long id;
+
+    private String username;
+    private String email;
+    //Todo or password hash?
+    private String password;
+    private LocalDateTime createdTime;
+
 }
