05e5e811a6f9ce7eb67418ff91602198a450ff81
Kawaiiskii <antonzhadnov@gmail.com>
2025-04-21 17:21:01 +0300

    implement entity Project

--- src/main/java/ru/tokyolifehard/taskmanager/entity/Project.java
+++ src/main/java/ru/tokyolifehard/taskmanager/entity/Project.java
@@ -1,4 +1,26 @@
 package ru.tokyolifehard.taskmanager.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Project {
+
+    @Id
+    @GeneratedValue()
+    private Long id;
+    private String name;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private Status status;
 }
