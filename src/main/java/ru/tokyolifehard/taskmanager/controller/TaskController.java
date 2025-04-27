b7066438c9e084993d912bdc54c05853365dfd1e
Kawaiiskii <antonzhadnov@gmail.com>
2025-04-27 21:57:23 +0300

    init methods in TaskService

--- src/main/java/ru/tokyolifehard/taskmanager/controller/TaskController.java
+++ src/main/java/ru/tokyolifehard/taskmanager/controller/TaskController.java
@@ -1,7 +1,36 @@
 package ru.tokyolifehard.taskmanager.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-
+@RestController
+@RequestMapping("/api/v1/tasks")
 public class TaskController {
+
+    @GetMapping
+    public ResponseEntity<?> getTask(){
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createTask(){
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateTask(){
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<?> modifyTask(){
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteTask(){
+        return ResponseEntity.ok().build();
+    }
+
+
 }
