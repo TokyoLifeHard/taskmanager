89623320cef2ecb6f60abbc6c2d9b738ab4cfba6
Kawaiiskii <antonzhadnov@gmail.com>
2025-04-29 19:41:50 +0300

    implement patch method

--- src/main/java/ru/tokyolifehard/taskmanager/utils/TaskDTOPatcher.java
+++ src/main/java/ru/tokyolifehard/taskmanager/utils/TaskDTOPatcher.java
@@ -1,4 +1,23 @@
 package ru.tokyolifehard.taskmanager.utils;
 
+import org.springframework.stereotype.Component;
+import ru.tokyolifehard.taskmanager.dto.TaskDTO;
+
+import java.lang.reflect.Field;
+
+@Component
 public class TaskDTOPatcher {
+    public static void patch(TaskDTO income,TaskDTO exists) throws IllegalAccessException {
+        Class<?> taskDtoClazz = TaskDTO.class;
+        Field[] declaredFields = taskDtoClazz.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            declaredField.setAccessible(true);
+            Object value = declaredField.get(income);
+            if(value!=null){
+                declaredField.set(exists,value);
+            }
+            declaredField.setAccessible(false);
+        }
+    }
+
 }
