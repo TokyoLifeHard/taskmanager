871201e588ce8e44a6328696d87ea3a5842cda3e
Kawaiiskii <antonzhadnov@gmail.com>
2025-05-26 19:00:53 +0300

    implement ProjectDTO

--- src/main/java/ru/tokyolifehard/taskmanager/dto/ProjectDTO.java
+++ src/main/java/ru/tokyolifehard/taskmanager/dto/ProjectDTO.java
@@ -1,4 +1,22 @@
 package ru.tokyolifehard.taskmanager.dto;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import ru.tokyolifehard.taskmanager.entity.Status;
+
+import java.time.Instant;
+@Getter
+@Setter
 public class ProjectDTO {
+    private String name;
+    private String description;
+    @Setter(AccessLevel.NONE)
+    private Instant startDate;
+    private Instant endDate;
+    private Status status;
+
+    public void setStartDate(Instant startDate) {
+        this.startDate = Instant.now();
+    }
 }
