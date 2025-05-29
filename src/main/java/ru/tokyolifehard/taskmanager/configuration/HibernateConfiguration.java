d2f3ace6557649fa01048ea7f1b2e30d450b4902
Kawaiiskii <antonzhadnov@gmail.com>
2025-05-29 22:46:49 +0300

    implemet HibernateConfiguration

--- src/main/java/ru/tokyolifehard/taskmanager/configuration/HibernateConfiguration.java
+++ src/main/java/ru/tokyolifehard/taskmanager/configuration/HibernateConfiguration.java
@@ -1,8 +1,70 @@
 package ru.tokyolifehard.taskmanager.configuration;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 public class HibernateConfiguration {
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
 
+
+    @Value("${spring.jpa.show-sql}")
+    private boolean showSql;
+    @Value("${spring.jpa.hibernate.dialect}")
+    private String dialect;
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String ddlAuto;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager
+                = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+    @Bean
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty(
+                "spring.jpa.hibernate.dialect", ddlAuto);
+        hibernateProperties.setProperty(
+                "spring.jpa.hibernate.ddl-auto", dialect);
+
+        return hibernateProperties;
+    }
 }
