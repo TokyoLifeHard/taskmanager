3fcff5cd6a2dc2039130ec41962fe3a403763e2d
Kawaiiskii <antonzhadnov@gmail.com>
2025-05-29 22:47:22 +0300

    init TaskMapper

---
+++ src/main/java/ru/tokyolifehard/taskmanager/mapper/TaskMapper.java
@@ -0,0 +1,9 @@
+package ru.tokyolifehard.taskmanager.mapper;
+
+import org.springframework.stereotype.Component;
+import ru.tokyolifehard.taskmanager.entity.Task;
+
+@Component
+public class TaskMapper {
+
+}
