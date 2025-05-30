f00a5703393e704d2e0968303e413f7310a97377
Kawaiiskii <antonzhadnov@gmail.com>
2025-05-30 23:53:57 +0300

    implement TaskMapper

--- src/main/java/ru/tokyolifehard/taskmanager/mapper/TaskMapper.java
+++ src/main/java/ru/tokyolifehard/taskmanager/mapper/TaskMapper.java
@@ -1,9 +1,33 @@
 package ru.tokyolifehard.taskmanager.mapper;
 
 import org.springframework.stereotype.Component;
+import ru.tokyolifehard.taskmanager.dto.TaskDTO;
 import ru.tokyolifehard.taskmanager.entity.Task;
 
 @Component
-public class TaskMapper {
+public class TaskMapper implements Mapper<Task,TaskDTO> {
 
+    @Override
+    public Task toEntity(TaskDTO dto) {
+        Task task = new Task();
+        task.setTitle(dto.getTitle());
+        task.setDescription(dto.getDescription());
+        task.setPriority(dto.getPriority());
+        task.setDueDate(dto.getLeadTime());
+        task.setCreatedAt(dto.getCreatedAt());
+        task.getProject().setName(dto.getProjectName());
+        return task;
+    }
+
+    @Override
+    public TaskDTO toDto(Task entity) {
+        TaskDTO taskDTO = new TaskDTO();
+        taskDTO.setTitle(entity.getTitle());
+        taskDTO.setDescription(entity.getDescription());
+        taskDTO.setPriority(entity.getPriority());
+        taskDTO.setCreatedAt(entity.getCreatedAt());
+        taskDTO.setLeadTime(entity.getCreatedAt());
+        taskDTO.setProjectName(entity.getProject().getName());
+        return taskDTO;
+    }
 }
