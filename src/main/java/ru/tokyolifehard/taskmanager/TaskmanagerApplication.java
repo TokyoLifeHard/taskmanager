7a1bffe5f2977b73f0bb1565d3339af5993b65a0
Kawaiiskii <antonzhadnov@gmail.com>
2025-06-02 23:42:27 +0300

    implement TaskService

--- src/main/java/ru/tokyolifehard/taskmanager/TaskmanagerApplication.java
+++ src/main/java/ru/tokyolifehard/taskmanager/TaskmanagerApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskmanagerApplication {
+//todo Переделать запросы, добвать джоины + добавть в dto результаты join сущностей
+//todo Решить проблему patch метода
+//todo Реализовать мапперы
+//todo Реализовать бизнес исключения
+//todo Реализовать логгирование
+//todo Реализовать сервис нотификаций
+//todo Реализовать сервис нотификаций
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmanagerApplication.class, args);
--- src/main/java/ru/tokyolifehard/taskmanager/service/TaskService.java
+++ src/main/java/ru/tokyolifehard/taskmanager/service/TaskService.java
@@ -1,4 +1,66 @@
 package ru.tokyolifehard.taskmanager.service;
 
+import org.springframework.stereotype.Service;
+import ru.tokyolifehard.taskmanager.dto.TaskDTO;
+import ru.tokyolifehard.taskmanager.entity.Task;
+import ru.tokyolifehard.taskmanager.mapper.TaskMapper;
+import ru.tokyolifehard.taskmanager.repository.TaskRepository;
+
+
+import java.lang.reflect.Field;
+
+@Service
 public class TaskService {
+
+
+    private TaskRepository taskRepository;
+    private TaskMapper taskMapper;
+
+    public TaskService(TaskRepository taskRepository, TaskMapper taskMapper) {
+        this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
+    }
+
+    public TaskDTO getTaskById(Long id) {
+        Task task = this.taskRepository.getTaskById(id);
+        return this.taskMapper.toDto(task);
+    }
+
+    public void createTask(TaskDTO taskDTO) {
+        this.taskRepository.createTask(taskDTO);
+    }
+
+    public void deleteById(Long id) {
+        this.taskRepository.deleteTaskById(id);
+    }
+
+    public void updateTask(Long id,TaskDTO taskDTO) {
+        this.taskRepository.updateTaskById(id,taskDTO);
+    }
+
+
+    public void modifyTask(Long id,TaskDTO incomeTaskDTO) {
+        Task taskById = this.taskRepository.getTaskById(id);
+        TaskDTO existsTaskDTO = this.taskMapper.toDto(taskById);
+        try {
+            patch(incomeTaskDTO,existsTaskDTO);
+        }catch (IllegalAccessException e){
+            e.getCause();
+        }
+        this.taskRepository.updateTaskById(id,existsTaskDTO);
+
+    }
+
+    private static void patch(TaskDTO income,TaskDTO exists) throws IllegalAccessException {
+        Class<?> taskDtoClazz = TaskDTO.class;
+        Field[] declaredFields = taskDtoClazz.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            declaredField.setAccessible(true);
+            Object value = declaredField.get(income);
+            if(value!=null){
+                declaredField.set(exists,value);
+            }
+            declaredField.setAccessible(false);
+        }
+    }
 }
